e5f450af094521f64515923cf3bd534fba0226ca
Olympe <olympe.marichal@gmail.com>
2025-12-08 15:39:28 +0100

    ajout intr02.cpp et avancement sur la page popcnt

--- site_web/asm_popcnt_32.php
+++ site_web/asm_popcnt_32.php
@@ -19,4 +19,23 @@ document_begin();
         de manipuler les données. Si on utilise un tableau de booléens, on aura la définition
         de données suivante :
     </p>
-    <?php do_geshi("code/asm_popcnt_32/", "cpp" ); ?>
+    <?php do_geshi("code/asm_popcnt_32/intro1.cpp", "cpp" ); ?>
+    <p>
+        La variable tab_records est un tableau d’enregistrements et tab_process un
+        tableau de booléens. Si la variable tab_process[i] est à true cela signifie que
+        l’enregistrement correspondant devra être pris en compte dans un traitement
+        ultérieur.
+        On pourra donc définir plusieurs méthodes liées au traitement du tableau tab_process :
+    </p>
+    <ul>
+        <li>void set(u32 n) qui met à jour le tableau tab_process afin d’indiquer que
+            l’enregistrement n doit être traité</li>
+        <li>void unset(u32 n) qui met à jour le tableau tab_process afin d’indiquer que
+            l’enregistrement n ne doit pas être traité</li>
+        <li>bool use(u32 n) qui retourne true si on doit traiter l’enregistrement n</li>
+        <li>u32 count() qui retourne le nombre d’enregistrements qui doivent être traités</li>
+    </ul>
+    <p>
+        Le code de ces sous-programmes est très simple et ressemble à ce qui suit :
+    </p>
+    <?php do_geshi("code/asm_popcnt_32/intro2.cpp", "cpp" ); ?>
